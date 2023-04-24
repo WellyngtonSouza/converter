c4135ed622477381ca40aca845ef374665a151d3
WellyngtonSouza <wellyngtonmonteiro84@gmail.com>
2023-04-24 19:09:19 -0300

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express"
+let app = express()
+import {router} from "./routes/route.js"
+
+app.set("view engine", "ejs")
+app.set("views", "./templates")
+
+app.use(router)
+
+
+app.listen(3000, (err)=>{
+    if(err){
+        console.log("ocorreu algum erro!!")
+    }else{
+        console.log("servidor criado com sucesso!!!")
+    }
+})
